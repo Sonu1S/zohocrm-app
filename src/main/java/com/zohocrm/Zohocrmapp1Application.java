62dd963fd5f4b8978ba171bc459977eda4ea8c3e
sonu <sonukrsharma1299@gmail.com>
2023-10-28 16:03:51 +0530

    2nd commit

--- src/main/java/com/zohocrm/Zohocrmapp1Application.java
+++ src/main/java/com/zohocrm/Zohocrmapp1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Zohocrmapp1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Zohocrmapp1Application.class, args);
+		System.out.println("Zoho CRM");
 	}
 
 }
